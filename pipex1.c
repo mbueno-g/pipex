fc6bb93203b45c10a427683e2c64cd4e7364c140
mbueno-g <buenogarciamarina@gmail.com>
2021-09-30 22:34:14 +0200

    FINALLY WORKING
    Fixed the (wc: 'standard input': Bad file descriptor) error by changing O_WRDONLY to O_RDONLY in the infile opening

---
+++ pipex1.c
@@ -0,0 +1,55 @@
+
+
+#include "pipex.h"
+
+
+void	child_process(t_cmd *cmd, char **envp)
+{
+	int fd[2];
+	pid_t pid;
+
+	pipe(fd);
+	pid = fork();
+	if (pid < 0)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	else if (pid == 0) //hijo
+	{
+		close(fd[READ_END]);
+		dup2(fd[WRITE_END], STDOUT);
+		execve(cmd->path, cmd->cmd_arg, envp);
+		//printf("holaa4\n");
+		//close(fd[WRITE_END]);
+	}
+	else
+	{
+		close(fd[WRITE_END]);
+		dup2(fd[READ_END], STDIN);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+
+void pipex(t_data *d, t_list *current, char **envp)
+{
+	int i;
+	t_cmd *aux;
+
+	i = 2;
+	dup2(d->fd_in, STDIN);
+	while (i <= d->argc - 2)
+	{
+		aux = (t_cmd *)current->content;
+		if (i == d->argc - 2)
+		{
+			dup2(d->fd_out, STDOUT);
+			execve(aux->path, aux->cmd_arg, envp);
+		}
+		else
+			child_process(aux, envp);
+		current = current->next;
+		i++;
+	} 
+}
