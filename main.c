daa8fce106bd8072ec802df209682b43c7e954b1
mbueno-g <buenogarciamarina@gmail.com>
2021-10-05 19:12:26 +0200

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbueno-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 19:08:40 by mbueno-g          #+#    #+#             */
-/*   Updated: 2021/10/05 19:10:09 by mbueno-g         ###   ########.fr       */
+/*   Updated: 2021/10/05 19:12:17 by mbueno-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	get_pathname(t_data *d, t_list **data, char *path, char **cmd_arg)
 	path_cmd = ft_strjoin(path_aux, *cmd_arg);
 	if (!path_cmd)
 	{
-		free(cmd_arg);
+		ft_free_matrix(&cmd_arg);
 		error(d, "Strjoin failed");
 	}
 	if (access(path_cmd, F_OK) != -1)
